663ea5b4d7b54b5e048a708137bdcf0ab7e113fe
aes-alienrip <73251414+aes-alienrip@users.noreply.github.com>
2025-01-19 19:03:31 +0800

    feat: Add Traditional Chinese (#39)
    * Update AppConfig.kt
    
    * Update strings.xml
    
    * Create strings.xml

--- app/src/main/java/com/parallelc/micts/config/AppConfig.kt
+++ app/src/main/java/com/parallelc/micts/config/AppConfig.kt
@@ -5,7 +5,8 @@ import java.util.Locale
 enum class Language(val id: Int, val toLocale: () -> Locale) {
     FollowSystem(R.string.follow_system, { Locale.getDefault() }),
     English(R.string.english, { Locale.ENGLISH }),
-    Chinese(R.string.chinese, { Locale.CHINESE })
+    SimplifiedChinese(R.string.simplified_chinese, { Locale.SIMPLIFIED_CHINESE }),
+    TraditionalChinese(R.string.traditional_chinese, { Locale.TRADITIONAL_CHINESE })
 }
 
 object AppConfig {
