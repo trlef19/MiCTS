5d045219da9dc1a3c934243054ae8785bd7b29b1
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2025-03-10 22:13:31 +0900

    feat: add japanese language (#42)
    * Add Japanese
    
    * Update AppConfig.kt
    
    ---------
    
    Co-authored-by: Parallelc <realparallelc@gmail.com>

--- app/src/main/java/com/parallelc/micts/config/AppConfig.kt
+++ app/src/main/java/com/parallelc/micts/config/AppConfig.kt
@@ -5,6 +5,7 @@ import java.util.Locale
 enum class Language(val id: Int, val toLocale: () -> Locale) {
     FollowSystem(R.string.follow_system, { Locale.getDefault() }),
     English(R.string.english, { Locale.ENGLISH }),
+    Japanese(R.string.japanese, { Locale.JAPANESE }),
     SimplifiedChinese(R.string.simplified_chinese, { Locale.SIMPLIFIED_CHINESE }),
     TraditionalChinese(R.string.traditional_chinese, { Locale.TRADITIONAL_CHINESE }),
     Vietnamese(R.string.vietnamese, { Locale("vi") })
